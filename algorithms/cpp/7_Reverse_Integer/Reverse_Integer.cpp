792d2dfcefa1b999433bf63db331de5bcce92430
Chunlin Wu <chunlin5@login3.stampede2.tacc.utexas.edu>
2019-06-28 17:10:29 -0500

    Reverse Integer

---
+++ algorithms/cpp/7_Reverse_Integer/Reverse_Integer.cpp
@@ -0,0 +1,18 @@
+#include  <iostream>
+using std::cout;
+using std::endl;
+
+class Solution {
+public:
+  int reverse(int x) {
+    int result{0};
+    while(x != 0){
+      if(abs(result) > INT_MAX/10){
+	return 0;
+      }
+      result = result*10 + x%10;
+      x = x/10;
+    }
+    return result;
+  }
+};
